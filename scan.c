1812d2c7b2682d805bc8bfd722bdb34516f74e7a
theaw3275 <west_thea@wheatoncollege.edu>
2025-10-19 17:20:24 -0400

    fixing, creating tarball

--- scan.c
+++ scan.c
@@ -10,17 +10,12 @@
 # include <stdlib.h>
 # include <string.h>
 
-char c; /* current char */
-
 void emit_token(FILE *fpOut, char *name);
 
 void print_output(char *tokenFile, char *codeFile);
 
-void check_letters(FILE *fp, FILE *fpIn);
-
-void check_digits(FILE *fp, FILE *fpIn);
-
 int main(int argc, char **argv) {
+        char c;
         FILE *fpIn;
         fpIn = fopen(argv[1], "r");
         if (!fpIn) return 1; /* something failed w/opening file */
@@ -33,10 +28,71 @@ int main(int argc, char **argv) {
         /* generalized loop for any lexeme */
         while (c != EOF) {
                 if (isalpha(c)) { /* we have an letter: abc...xyzABC...XYZ */
-                        check_letters(fp, fpIn);
+                        int cap = 20;
+                        int len = 0;
+                        char *s = (char *) malloc (cap * sizeof (char));
+                        if (!s){
+                                printf("Memory allocation failed\n");
+                                return 1; /* something failed w/allocation */ 
+                        }
+                        while (isalpha(c)) {
+                                if (len + 1 >= cap){ /* allocate more space if needed */
+                                        cap = cap + 20;
+                                        s = (char *) realloc(s, cap * sizeof (char));
+                                }
+                                s[len] = c;
+                                len++;
+                                c = getc(fpIn);
+                        }
+                        if (strcmp(s, "program") == 0)
+                                emit_token(fp, "PROGRAM");
+                        else if (strcmp(s, "begin") == 0)
+                                emit_token(fp, "BEGIN");
+                        else if (strcmp(s, "end") == 0)
+                                emit_token(fp, "END");
+                        else if (strcmp(s, "int") == 0)
+                                emit_token(fp, "INT-TYPE");
+                        else if (strcmp(s, "float") == 0)
+                                emit_token(fp, "FLOAT-TYPE");
+                        else if (strcmp(s, "if") == 0)
+                                emit_token(fp, "IF");
+                        else if (strcmp(s, "else") == 0)
+                                emit_token(fp, "ELSE");
+                        else if (strcmp(s, "while") == 0)
+                                emit_token(fp, "WHILE");
+                        else
+                                emit_token(fp, "ID");
+                        free(s);
                 }
                 else if (isdigit(c)) { /* we have a digit: 0...9 */
-                        check_digits(fp, fpIn);
+                        int inum = 0;
+                        float fnum;
+                        float decPlace = 0.1;
+                        while (isdigit(c)) {
+                                /* we have a number */
+                                inum = inum * 10 + (c - 48);
+                                c = getc(fpIn);
+                        }
+                        if (c == '.') {
+                                /* we have a float */
+                                fnum = inum;
+                                c = getc(fpIn);
+                                if (isdigit(c)){
+                                        while (isdigit(c)) {
+                                                fnum = fnum + (c-48) * decPlace;
+                                                decPlace /= 10;
+                                                c = getc(fpIn);
+                                        }
+                                        emit_token(fp, "FLOAT-CONST");
+                                }
+                                else{
+                                        emit_token(fp, "INT-CONST");
+                                        emit_token(fp, "UNDEF");
+                                }
+                                
+                        }
+                        else
+                                emit_token(fp, "INT-CONST");
                 }
                 else if (c == ';'){
                         emit_token(fp, "STMT-END");
@@ -138,75 +194,6 @@ void emit_token(FILE *fpOut, char *name) {
                 fputs(name, fpOut);
                 fputc(' ', fpOut);
         }
-}
-
-void check_letters(FILE *fp, FILE *fpIn){
-        int cap = 20;
-	int len = 0;
-        char *s = (char *) malloc (cap * sizeof (char));
-	if (!s){
-                printf("Memory allocation failed\n");
-                return; /* something failed w/allocation */ 
-        }
-        while (isalpha(c)) {
-	        if (len + 1 >= cap){ /* allocate more space if needed */
-	        	cap = cap + 20;
-	        	s = (char *) realloc(s, cap * sizeof (char));
-		}
-                s[len] = c;
-                len++;
-                c = getc(fpIn);
-        }
-        if (strcmp(s, "program") == 0)
-                emit_token(fp, "PROGRAM");
-        else if (strcmp(s, "begin") == 0)
-                emit_token(fp, "BEGIN");
-        else if (strcmp(s, "end") == 0)
-                emit_token(fp, "END");
-        else if (strcmp(s, "int") == 0)
-                emit_token(fp, "INT-TYPE");
-        else if (strcmp(s, "float") == 0)
-                emit_token(fp, "FLOAT-TYPE");
-        else if (strcmp(s, "if") == 0)
-                emit_token(fp, "IF");
-        else if (strcmp(s, "else") == 0)
-                emit_token(fp, "ELSE");
-        else if (strcmp(s, "while") == 0)
-                emit_token(fp, "WHILE");
-        else
-                emit_token(fp, "ID");
-        free(s);      
-}
-
-void check_digits(FILE *fp, FILE *fpIn){
-        int inum = 0;
-                float fnum;
-                float decPlace = 0.1;
-                while (isdigit(c)) {
-                        /* we have a number */
-                        inum = inum * 10 + (c - 48);
-                        c = getc(fpIn);
-                }
-                if (c == '.') {
-                        /* we have a float */
-                        fnum = inum;
-                        c = getc(fpIn);
-                        if (isdigit(c)){
-                                while (isdigit(c)) {
-                                        fnum = fnum + (c-48) * decPlace;
-                                        decPlace /= 10;
-                                        c = getc(fpIn);
-                                }
-                                emit_token(fp, "FLOAT-CONST");
-                        }
-                        else{
-                                emit_token(fp, "INT-CONST");
-                                emit_token(fp, "UNDEF");
-                        }
-                        
-                }
-                else
-                        emit_token(fp, "INT-CONST");
 }
 
 void print_output(char *tokenFile, char *codeFile){
